06e658975f4bf852a7809b411c198ef6d329f53e
bryan <24bryan.jonathan@gmail.com>
2024-06-08 03:36:59 +0800

    Tidied up some code

--- src/components/buttons/button.tsx
+++ src/components/buttons/button.tsx
@@ -1,17 +1,11 @@
 import React from "react";
-import clsx from "clsx";
 
 import { buttonProps } from "@/types";
 
 const Button = ( {type, text, onClick, actionButton, className, bgColor, ...props} : buttonProps) => {
     return(
         <div className={className}>
-            <button onClick={onClick} type={type} 
-                // className={clsx(
-                // actionButton && "text-white hover:bg-blue-800 focus:ring-4 font-medium founded-lg text-small px-5 py-2.5 me-2 mb-2 focus:outline-none",
-                // `${bgColor} hover:${bgColor} font-medium founded-lg text-small px-5 py-2.5 me-2 mb-2 focus:outline-none`
-                // )}'
-            >
+            <button onClick={onClick} type={type}>
                 {text}
             </button>
         </div>
